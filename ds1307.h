422d015fdb2df47413fed28f29312676846517b1
Kolya <kolyazoloto@mail.ru>
2019-03-27 23:34:07 +0800

    v0.2

--- ds1307.h
+++ ds1307.h
@@ -2,14 +2,61 @@
 #define DS1307_H_
 #include "main.h"
 #include "I2C.h"
-
-unsigned char fromDecToBinDec(unsigned char b){ //Перевод из десятичного вида в двоично-десятичный
-	unsigned char h_b = b/10;//Десятки
-	unsigned char l_b = b%10;//Единицы
-	return (h_b<<4)+l_b;
-}
-unsigned char fromBinDecToDec(unsigned char b){  //Перевод из двоично-десятичный вида в десятичный
-	return (((b>>4)&0b00001111)*10)+(b&0b00001111);  //Делаем десятки и единицы и соединяем
-}
+class ds1307{
+public:
+	ds1307(){
+		i2c_init();
+	}
+	void set_datetime(unsigned char year,month,date,day,hour,min,sec){
+		i2c_init();
+		i2c_start();
+		i2c_sendbyte(0b11010000);
+		i2c_sendbyte(0);
+		i2c_sendbyte(fromDecToBinDec(sec));//секунды
+		i2c_sendbyte(fromDecToBinDec(min));//минуты
+		i2c_sendbyte(fromDecToBinDec(hour));//часы
+		i2c_sendbyte(fromDecToBinDec(day));//день
+		i2c_sendbyte(fromDecToBinDec(date));//дата
+		i2c_sendbyte(fromDecToBinDec(month));//месяц
+		i2c_sendbyte(fromDecToBinDec(year));//год
+		i2c_stop();
+	}
+	void get_datetime(){
+		i2c_start();
+		i2c_sendbyte(0b11010000);// Записываем в какую область памяти переходим
+		i2c_sendbyte(0);//
+		i2c_stop();
+		
+		i2c_start();
+		i2c_sendbyte(0b11010001);// Включаем режим чтения
+		sec = I2C_ReadByte();
+		min = I2C_ReadByte();
+		hour = I2C_ReadByte();
+		day = I2C_ReadByte();
+		date = I2C_ReadByte();
+		month = I2C_ReadByte();
+		year = I2C_ReadLastByte();
+		i2c_stop();
+		
+		sec = fromBinDecToDec(sec);
+		min = fromBinDecToDec(min);
+		hour = fromBinDecToDec(hour);
+		day = fromBinDecToDec(day);
+		date = fromBinDecToDec(date);
+		month = fromBinDecToDec(month);
+		year = fromBinDecToDec(year);
+	}
+	
+	unsigned char sec,min,hour,day,date,month,year;
+private:
+	unsigned char fromDecToBinDec(unsigned char b){ //Перевод из десятичного вида в двоично-десятичный
+		unsigned char h_b = b/10;//Десятки
+		unsigned char l_b = b%10;//Единицы
+		return (h_b<<4)+l_b;
+	}
+	unsigned char fromBinDecToDec(unsigned char b){  //Перевод из двоично-десятичный вида в десятичный
+		return (((b>>4)&0b00001111)*10)+(b&0b00001111);  //Делаем десятки и единицы и соединяем
+	}
+};
 
 #endif /* DS1307_H_ */
--- main.cpp
+++ main.cpp
@@ -7,72 +7,24 @@
 
 #include "main.h"
 
-int main(void)
-{
-	/*i2c_init();
-	i2c_start();
-	i2c_sendbyte(0b11010000);
-	i2c_sendbyte(0);
-	i2c_sendbyte(fromDecToBinDec(00));//секунды
-	i2c_sendbyte(fromDecToBinDec(17));//минуты
-	i2c_sendbyte(fromDecToBinDec(19));//часы
-	i2c_sendbyte(fromDecToBinDec(3));//день
-	i2c_sendbyte(fromDecToBinDec(27));//дата
-	i2c_sendbyte(fromDecToBinDec(3));//месяц
-	i2c_sendbyte(fromDecToBinDec(19));//год
-	i2c_stop();*/
-	
-	LCD_1602_I2C lcd(0x4E);
-	i2c_init();
-	
-	
+int main(void){
+	LCD_1602_I2C lcd;
+	ds1307 clock;
+	//clock.set_datetime(25,12,5,6,12,25,0);
 
-	unsigned char sec;
-	unsigned char min;
-	unsigned char hour;
-	unsigned char day;
-	unsigned char date;
-	unsigned char month;
-	unsigned char year ;
     while (1) 
-    {
+    {	
+		clock.get_datetime();
 		lcd.move_cursor(1,1);
-		i2c_start();
-		i2c_sendbyte(0b11010000);//вначале нужно записать куда в память переходим
-		i2c_sendbyte(0);//
-		i2c_stop();
-	
-		
-		i2c_start();
-		i2c_sendbyte(0b11010001);
-		sec = I2C_ReadByte();
-		min = I2C_ReadByte();
-		hour = I2C_ReadByte();
-		day = I2C_ReadByte();
-		date = I2C_ReadByte();
-		month = I2C_ReadByte();
-		year = I2C_ReadLastByte();
-		i2c_stop();
-		
-		sec = fromBinDecToDec(sec);
-		min = fromBinDecToDec(min);
-		hour = fromBinDecToDec(hour);
-		day = fromBinDecToDec(day);
-		date = fromBinDecToDec(date);
-		month = fromBinDecToDec(month);
-		year = fromBinDecToDec(year);
-		lcd.print(hour/10+'0');
-		lcd.print(hour%10+'0');
+		lcd.print(clock.hour/10+'0');
+		lcd.print(clock.hour%10+'0');
 		lcd.print(':');
-		lcd.print(min/10+'0');
-		lcd.print(min%10+'0');
+		lcd.print(clock.min/10+'0');
+		lcd.print(clock.min%10+'0');
 		lcd.print(':');
-		lcd.print(sec/10+'0');
-		lcd.print(sec%10+'0');
-		lcd.move_cursor(2,1);
-		lcd.print(date/10+'0');
-		lcd.print(date%10+'0');
-
+		lcd.print(':');
+		lcd.print(clock.sec/10+'0');
+		lcd.print(clock.sec%10+'0');
 		
     }
 }
